26bb88be67dded619581d54a9944a8c46421774b
APatra <43411221+desiFish@users.noreply.github.com>
2025-02-17 10:20:18 +0530

    updates

--- fanService.sh
+++ fanService.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+# fanService.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
 cd /
 cd /home/admin/projects/RPiFan/
-sudo python3 ./fan_control.py &
+sudo python3 ./fan_control.py
 cd /
